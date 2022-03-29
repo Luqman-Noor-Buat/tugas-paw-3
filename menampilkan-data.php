9b55024c2bc26873bd8e7daf4307c5387daf7123
Luqman-Noor-Buat <luqman.utm1@gmail.com>
2022-03-29 13:15:19 +0700

    Menampilkan data di tabel tbl_206 dari database luqman

---
+++ menampilkan-data.php
@@ -0,0 +1,17 @@
+<?php
+
+include "koneksi.php";
+
+$sql = "SELECT * FROM tbl_206";
+$hasil = mysqli_query($koneksi, $sql);
+while($row = mysqli_fetch_array($hasil))
+{
+    echo $row["nim"];
+    echo $row["nama"];
+    echo $row["jenis_kelamin"];
+    echo $row["prodi"];
+    echo $row["asal_kota"];
+}
+
+
+?>
