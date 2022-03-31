6305131fcf6aa5df9388a85338154b4fc88b4ee7
Luqman-Noor-Buat <luqman.utm1@gmail.com>
2022-03-31 22:18:08 +0700

    Membuat file untuk mengubah menjadi data yang terbaru

---
+++ ubah.php
@@ -0,0 +1,31 @@
+<?php
+include "koneksi.php";
+$nim = $_POST['nim'];
+$nama = $_POST['nama'];
+$jk = $_POST['jenis_kelamin'];
+$prodi = $_POST['prodi'];
+$asal = $_POST['asal_kota'];
+
+$sql ="UPDATE tbl_206 SET
+        nama = '$nama',
+        jenis_kelamin = '$jk',
+        prodi = '$prodi',
+        asal_kota = '$asal' 
+        WHERE nim=$nim";
+$hasil = mysqli_query($koneksi, $sql);
+if(!$hasil){
+    echo "
+    <script>
+      alert('Anda gagal memperbarui data, silahkan coba kembali :(');
+      document.location.href='mengubah-data.php';   
+    </script>
+    ";
+}else{
+    echo "
+    <script>
+      alert('Anda telah berhasil mengubah data');
+      document.location.href='menampilkan-data.php';   
+    </script>";
+}
+
+?>
