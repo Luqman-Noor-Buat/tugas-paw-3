622f43e3b8e947e7ee87a4153dcd8338446b16b4
Luqman-Noor-Buat <luqman.utm1@gmail.com>
2022-03-31 22:17:28 +0700

    Memperbarui judul

--- menampilkan-data.php
+++ menampilkan-data.php
@@ -14,7 +14,7 @@ $hasil = mysqli_query($koneksi, $sql);
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    <title>Menampilkan Data</title>
+    <title>Mengubah Data</title>
   </head>
   <body>
     <!--<h1>Lagi Belajar Bootstrap!</h1>-->
